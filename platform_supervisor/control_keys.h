343e4969a7cf2aa116a4603833cefb6228c7da9d
stanislavr <Srashevskyi8178@conestogac.on.ca>
2016-10-19 20:04:29 -0400

    Added Keyboard driving commands [ To Be Tested ]

---
+++ platform_supervisor/control_keys.h
@@ -0,0 +1,11 @@
+/*
+ * control_keys.h
+ *
+ *  Created on: October 19, 2016
+ *      Author: Stanislav Rashevskyi
+ *      Purpose: header for control_keys.c
+ *
+ */
+
+#include <ncurses.h>
+#define DEBUG_ARROWS 1  // enable debug printfs
